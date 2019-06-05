d75c16e780c20f065e704c937ec954007cc62146
wrusselly <w.russelly@gmail.com>
2019-06-05 11:16:37 +0100

    Update Login.vue

--- ci-project-dist/authentication-client/src/components/Login.vue
+++ ci-project-dist/authentication-client/src/components/Login.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
     <b-form class="mx-auto" @submit="submit">
-        <h1>Hello World</h1>
+        <h1>rolling update</h1>
         <b-form-input 
             v-model="form.email"
             type="email"
